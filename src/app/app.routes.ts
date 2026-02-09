d453b177390616092ca3b0f03b76cef536816d0e
Ivan Yurkov <youzik1999@gmail.com>
2026-02-09 23:16:07 +0300

    add filter style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,29 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard, guestGuard } from './core/auth.guard';
 import { App } from './app';
-import { FormComponent } from './components/form/form.component';
-import { BaseTableService } from './services/base-table.service';
 import { UsersService } from './services/users.service';
 import { SignupComponent } from './components/signup/signup.component';
 import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
 import { SpravkaComponent } from './components/spravka/spravka.component';
 import { TableComponent } from './components/table/table.component';
 import { OrdersService } from './services/orders.service';
-import { ORDERS_CONFIG } from './core/columnsConfigs';
+import { DEALERS_CONFIG, DETAILS_CONFIG, DETAILS_FOR_ORDER_CONFIG, DEVICES_CONFIG, DEVICES_TYPES_CONFIG, ORDERS_CONFIG, USERS_CONFIG, WORKERS_CONFIG } from './core/columnsConfigs';
 import { inject } from '@angular/core';
+import { WorkersService } from './services/workers.service';
+import { DevicesService } from './services/devices.service';
+import { DetailsService } from './services/details.service';
+import { DealersService } from './services/dealers.service';
+import { DetailsForOrderService } from './services/details-for-order.service';
+import { DevicesTypesService } from './services/devices-types.service';
 
 const ordersServiceResolver = () => inject(OrdersService);
+const usersServiceResolver = () => inject(UsersService);
+const workersServiceResolver = () => inject(WorkersService);
+const devicesServiceResolver = () => inject(DevicesService);
+const detailsServiceResolver = () => inject(DetailsService);
+const dealersServiceResolver = () => inject(DealersService);
+const detailsForOrderServiceResolver = () => inject(DetailsForOrderService);
+const devicesTypesServiceResolver = () => inject(DevicesTypesService);
 
 export const routes: Routes = [
     {
@@ -51,7 +62,63 @@ export const routes: Routes = [
               tableService: ordersServiceResolver,
             },
             data: { config: ORDERS_CONFIG }
-          }
+          },
+          {
+            path: 'users',
+            component: TableComponent,
+            resolve: {
+              tableService: usersServiceResolver,
+            },
+            data: { config: USERS_CONFIG }
+          },
+          {
+            path: 'workers',
+            component: TableComponent,
+            resolve: {
+              tableService: workersServiceResolver,
+            },
+            data: { config: WORKERS_CONFIG }
+          },
+          {
+            path: 'devices',
+            component: TableComponent,
+            resolve: {
+              tableService: devicesServiceResolver,
+            },
+            data: { config: DEVICES_CONFIG }
+          },
+          {
+            path: 'details',
+            component: TableComponent,
+            resolve: {
+              tableService: detailsServiceResolver,
+            },
+            data: { config: DETAILS_CONFIG }
+          },
+          {
+            path: 'dealers',
+            component: TableComponent,
+            resolve: {
+              tableService: dealersServiceResolver,
+            },
+            data: { config: DEALERS_CONFIG }
+          },
+          {
+            path: 'details-for-order',
+            component: TableComponent,
+            resolve: {
+              tableService: detailsForOrderServiceResolver,
+            },
+            data: { config: DETAILS_FOR_ORDER_CONFIG }
+          },
+          {
+            path: 'devices-types',
+            component: TableComponent,
+            resolve: {
+              tableService: devicesTypesServiceResolver,
+            },
+            data: { config: DEVICES_TYPES_CONFIG }
+          },
         ]
       },
       {
